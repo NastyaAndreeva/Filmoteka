03ff6cd766d10c1a2848b2c86e90785e4084ac4c
Daniel254 <Daniel.laskin@gmail.com>
2022-06-07 22:35:29 +0300

    save movie ids to firestore

---
+++ src/js/utils/firebase_app.js
@@ -0,0 +1,10 @@
+import { initializeApp } from 'firebase/app';
+export default app = initializeApp({
+    apiKey: "AIzaSyAN-5I6EVm1JCDsJFVlHPFOnqDlUOfsbaw",
+    authDomain: "filmoteka-98189.firebaseapp.com",
+    databaseURL: "https://filmoteka-98189-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "filmoteka-98189",
+    storageBucket: "filmoteka-98189.appspot.com",
+    messagingSenderId: "437648861220",
+    appId: "1:437648861220:web:0545ac1cdbb5bb833190b1"
+});
