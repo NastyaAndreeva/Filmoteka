a9925ec71154b24c23ba15ee5e0fea5b2d9c7e3c
NastyaAndreeva <44640535+NastyaAndreeva@users.noreply.github.com>
2022-06-08 17:49:37 +0000

    Deploying to gh-pages from @ NastyaAndreeva/filmoteka@98de659e85c317e2b502b2958f4b7e2c78259dc8 🚀

--- library.f12261f1.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("#dark-theme"),t=document.querySelector(".body-theme");"dark-theme"===localStorage.getItem("color-theme")&&t.classList.add("dark-theme"),e.addEventListener("click",(()=>{t.classList.toggle("dark-theme"),document.querySelector(".dark-theme")?localStorage.setItem("color-theme","dark-theme"):localStorage.removeItem("color-theme")}));
-//# sourceMappingURL=library.f12261f1.js.map
