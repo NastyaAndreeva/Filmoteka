ec6dafe342059286b95d52de4e65276641002766
andrey-korchevniy <91483336+andrey-korchevniy@users.noreply.github.com>
2022-06-10 16:31:29 +0300

    готово без пагинации

--- src/index.js
+++ src/index.js
@@ -6,18 +6,40 @@ import {
   getGenresIds,
 } from './js/movie-fetch';
 
+// import { initLightbox } from './js/modal-film.js';
 import itemsTemplate from './templates/list-of-card.hbs';
 
-const gallery = document.querySelector('.gallery');
+const form = document.querySelector("form");
+const gallery = document.querySelector('#home-gallery');
 
-async function generateMurkup() {
+async function generateMarkup() {
   const moviesData = await getTrendingMoviesData();
+
+  // Creating an object that stores data for handlebars template
+  const movieCategories = await generateMoviesWithGenres(moviesData);
+
+  // Rendering markup
+  // gallery.innerHTML = itemsTemplate(movieCategories);
+}
+
+async function onSearchSubmit(event) {
+  event.preventDefault();
+
+  const moviesData = await getDataMovies(event.currentTarget.elements.searchQuery.value);
+
+  const movieCategories = await generateMoviesWithGenres(moviesData);
+
+  // Rendering markup
+  gallery.innerHTML = itemsTemplate(movieCategories);
+}
+
+async function generateMoviesWithGenres(data){
   const genres = await getGenresIds();
 
-  // Создание объекта в котором хранится информация для Handlebars
-  const movieCategories = moviesData.results.map(movie => {
+  // Creating an object that stores data for handlebars template
+  return data.results.map(movie => {
     const catArr = [];
-    const dataRelease = movie.release_date.slice(0, 4);
+    const dataRelease = movie.release_date?.slice(0, 4) || 2022;
     const nameOfFilm = movie.title.toUpperCase();
     const movieInfo = {
       name: nameOfFilm,
@@ -28,7 +50,7 @@ async function generateMurkup() {
       backdrop_path: movie.backdrop_path,
     };
 
-    // Сравнивает жанры из массива жанров с id жанров из общего запроса и добавляет нужные в объект для Handlebars
+    // Comparing ganres taken from the general ganre array with IDs and adding ganres' values in the object for handlebars template
     const genresFilm = function () {
       movie.genre_ids.map(id =>
         genres.find(el => {
@@ -42,90 +64,9 @@ async function generateMurkup() {
     genresFilm();
     return movieInfo;
   });
-
-  // Создание разметки
-  gallery.insertAdjacentHTML('beforeend', itemsTemplate(movieCategories));
 }
 
+generateMarkup();
 
+// form.addEventListener("submit", onSearchSubmit);
 
-generateMurkup();
-
-////////////////////////// Firebase //////////////////////////
-import currentUser from './js/storage/currentUser';
-import apiFirebase from './js/api/firebase';
-console.log(apiFirebase);
-
-document.querySelector('.header-library__btnQue').addEventListener('click', () => {
-  console.log(userMovies.queue);
-});
-document.querySelector('.header-library__btnWatc').addEventListener('click', () => {
-  console.log(userMovies.wathced);
-});
-
-//!-- need to delete
-//From temp partial
-const container = document.querySelector('.container');
-const login = document.querySelector('.login');
-const logout = document.querySelector('.logout');
-const add_queue = document.querySelector('.add_queue');
-const add_watched = document.querySelector('.add_watched');
-const get = document.querySelector('.get');
-const stat = document.querySelector('.stat');
-//--END from temp partial
-
-//login click callback
-function onLoginClick() {
-  console.log('login');
-  apiFirebase.auth.logInByGoogle();
-  // authGoogleAPI
-  //   .LogInByGoogle()
-  //   .then(result => {
-  //     const user = result.user;
-  //     console.log(user);
-  //     console.log(user.displayName);
-  //   })
-  //   .catch(error => {
-  //     const credential = GoogleAuthProvider.credentialFromError(error);
-  //     // ...
-  //     console.log('catch ', error, 'credential ', credential);
-  //   });
-}
-//Вход
-login.addEventListener('click', onLoginClick);
-//Выход
-logout.addEventListener('click', () => {
-  console.log('logout');
-  apiFirebase.auth.logOut();
-});
-//Запись в базу данных
-add_queue.addEventListener('click', () => {
-  console.log('add queue');
-  apiFirebase.add.addToQueue(Math.random());
-  // Передаем методу два массива - первый очередь, второй просмотренные
-  // authGoogleAPI.addDocument([1, 2, 3], [4, 5, 6]);
-});
-add_watched.addEventListener('click', () => {
-  console.log('add watched');
-  apiFirebase.add.addToWatched(Math.random());
-});
-//Выборка данных из базы
-// get.addEventListener('click', () => {
-//   console.log('get');
-//   const usr = authGoogleAPI.getDocument();
-//   //Получаем промис с результатом в виде объекта с двумя массивами - очередь и просмотренные
-//   usr
-//     .then(result => {
-//       console.log('Result ', result);
-//     })
-//     .catch(error => {
-//       console.log(error);
-//     });
-// });
-//!END -- need to delete
-stat.addEventListener('click', () => {
-  console.log('stat');
-  console.log(currentUser);
-});
-
-// UserClass.authStateListener();
