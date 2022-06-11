009897a124b232d241d5dbda02a8b938ea6e2f6c
andrey-korchevniy <91483336+andrey-korchevniy@users.noreply.github.com>
2022-06-11 19:04:41 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
+import './js/pagination';
 import {
   getDataMovies,
   getMoreDataMovies,
   getTrendingMoviesData,
   getMoreTrendingMoviesData,
   getGenresIds,
+  getOneMovieById,
 } from './js/movie-fetch';
 
-// import { initLightbox } from './js/modal-film.js';
+import { initLightbox } from './js/modal-film.js';
 import itemsTemplate from './templates/list-of-card.hbs';
+import preloader from './templates/preloader.hbs';
+
+const gallery = document.querySelector('.gallery');
+const preloaderContainer = document.querySelector(".preloader");
+preloaderContainer.innerHTML = preloader();
 
 const form = document.querySelector("form");
-const gallery = document.querySelector('#home-gallery');
 
 async function generateMarkup() {
   const moviesData = await getTrendingMoviesData();
@@ -19,21 +25,31 @@ async function generateMarkup() {
   const movieCategories = await generateMoviesWithGenres(moviesData);
 
   // Rendering markup
-  // gallery.innerHTML = itemsTemplate(movieCategories);
+  setTimeout(() => {
+    preloaderContainer.innerHTML = '';
+    gallery.insertAdjacentHTML('beforeend', itemsTemplate(movieCategories));
+    footer.style.position = "static";
+  }, 2000)
+
 }
 
 async function onSearchSubmit(event) {
   event.preventDefault();
 
-  const moviesData = await getDataMovies(event.currentTarget.elements.searchQuery.value);
+  const moviesData = await getDataMovies(
+    event.currentTarget.elements.searchQuery.value
+  );
 
   const movieCategories = await generateMoviesWithGenres(moviesData);
 
   // Rendering markup
+
   gallery.innerHTML = itemsTemplate(movieCategories);
+
+  
 }
 
-async function generateMoviesWithGenres(data){
+async function generateMoviesWithGenres(data) {
   const genres = await getGenresIds();
 
   // Creating an object that stores data for handlebars template
@@ -68,5 +84,4 @@ async function generateMoviesWithGenres(data){
 
 generateMarkup();
 
-// form.addEventListener("submit", onSearchSubmit);
-
+form.addEventListener('submit', onSearchSubmit)
